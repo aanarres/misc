15a11b5ceaa1e9b578de8a036831f4a9f89a61ce
anarres <aanarres@gmail.com>
2016-06-18 13:52:59 +0200

    JavaScript has no style, or that moment when you wish you'd used a coding style

---
+++ no_style.js
@@ -0,0 +1,26 @@
+// NO-STYLE js
+
+// Do foo1() and foo2() below do the same thing?
+// The only difference is starting the bracket on the same line or next.
+// It's just that js has no style, that is, this isn't a matter of coding style:
+// foo2() completes with the return line with the optional ; soooooo it returns lol
+
+function foo1()
+{
+  return {
+      bar: "hello"
+  };
+}
+
+function foo2()
+{
+  return
+  {
+      bar: "hello"
+  };
+}
+
+console.log("foo1 returns:");
+console.log(foo1());
+console.log("foo2 returns:");
+console.log(foo2());
