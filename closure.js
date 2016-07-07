44bffcb269f8b7f4d68c64547bf7bc891a08a880
anarres <aanarres@gmail.com>
2016-07-07 22:46:28 +0200

    Add another simple closure example.

--- closure.js
+++ closure.js
@@ -1,4 +1,6 @@
 /**
+ * Scopes and closures, from toptal.com
+ *
  * Closure: visibility of variables and treatment of globals
  * The first function will execute as the file is run, the second is a named function that we call.
  * In both cases, the global trick works the same way: b and b2 become accessible globally.
@@ -29,3 +31,37 @@ b = 5
 a2 = undefined
 b2 = 5
 */
+
+/**
+ * Closure: inner function that has access to vars in the outer, enclosing,
+ * function scope. Plus them globals, but don't quote me on this one,
+ * for everybody knows globals are Evil.
+*/
+
+var globalVar = "Evil";
+
+(function outerFunc(outerArg) {
+  var outerVar = 'anOuter';
+  
+  (function innerFunc(innerArg) {
+    var innerVar = 'anInner';
+    
+    console.log(
+      "outerArg = " + outerArg + "\n" +
+      "innerArg = " + innerArg + "\n" +
+      "outerVar = " + outerVar + "\n" +
+      "innerVar = " + innerVar + "\n" +
+      "globalVar = " + globalVar);
+    
+  })(17);
+})(99);
+
+/*
+ Will display:
+outerArg = 17
+innerArg = 99
+outerVar = anOuter
+innerVar = anInner
+globalVar = Evil
+*/
+
